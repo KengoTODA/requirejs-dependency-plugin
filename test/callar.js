4318abfaf0589e067d6fe8e4f7517e2c29270130
Kengo TODA <skypencil@gmail.com>
2013-02-14 22:45:54 +0800

    temporary

--- test/callar.js
+++ test/callar.js
@@ -1,3 +1,3 @@
-define(["callee"], function(callee) {
+define(["https://raw.github.com/eller86/requirejs-dependency-plugin/dot-format/test/callee.js"], function(callee) {
   equal(callee.dummy, 0);
 });
